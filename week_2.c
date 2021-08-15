41e1663f78aba32385d7f8a2b8145d9a87a96fb9
Pattanapon Chaiwongsa <aof.pattanapon@gmail.com>
2021-08-16 06:28:52 +0700

    Final

--- week_2.c
+++ week_2.c
@@ -1,13 +1,17 @@
 #include<stdio.h>
 int main(){
     int a,b;
+    float i;
     printf("Enter first number : ");
     scanf("%d", &a);
     printf("Enter second number : ");
     scanf("%d",&b);
+    i = (float)a/b;
 
     printf("Your result is : %d + %d = %d\n",a,b,a+b);
     printf("\t\t%d - %d = %d\n",a,b,a-b);
+    printf("\t\t%d x %d = %d\n",a,b,a*b);
+    printf("\t\t%d / %d = %.07f",a,b,i);
     
 
 
